c2479654eb8709887327e398f6ba263a12187931
renxianchao <renxianchao@51jianku.com>
2019-12-31 20:35:24 +0800

    验收表单

--- src/views/land/examine/business/compoment/appceptance-form.vue
+++ src/views/land/examine/business/compoment/appceptance-form.vue
@@ -1,15 +1,170 @@
 <template>
   <div>
-      <h1>验收表单</h1>
+    <el-form>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="申请户主">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="身份证号">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="乡村建设规划许可证">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="农村宅基地批准书号">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="开工日期">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="竣工日期">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="批准宅基地面积">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="实用宅基地面积">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="批准房基占地面积">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="实际房基占地面积">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="批准层数">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="竣工层数">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="批准高度">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="竣工高度">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="拆旧退还宅基地情况">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="竣工平面简图经办人">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="农业农村部门验收意见">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="农业农村部门验收经办人">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="农业农村部门验收时间">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="自然资源部门验收意见">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="自然资源部门验收经办人">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="自然资源部门验收时间">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="乡镇政府验收意见">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="乡镇政府验收经办人">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="乡镇政府验收时间">
+            <el-input size="small"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-    name:'appceptance-form'
-}
+  name: "appceptance-form"
+};
 </script>
 
 <style>
-
 </style>
