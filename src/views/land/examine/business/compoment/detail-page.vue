06603298e3d0cfd207116d36293eb69c80b65b97
杨晗 <yanghan@51jianku.com>
2020-01-03 14:37:06 +0800

    详情页添加表单可控编辑

--- src/views/land/examine/business/compoment/detail-page.vue
+++ src/views/land/examine/business/compoment/detail-page.vue
@@ -8,13 +8,13 @@
         <el-divider></el-divider>
         <el-tabs tab-position="left">
             <el-tab-pane label="申请表">
-                <applicationForm></applicationForm>
+                <applicationForm :disabled="applicationFormDisabled" :detail="detail"></applicationForm>
             </el-tab-pane>
             <el-tab-pane label="审批表">
-                <approvalForm></approvalForm>
+                <approvalForm :disabled="appceptanceFormDisabled" :detail="detail"></approvalForm>
             </el-tab-pane>
             <el-tab-pane label="验收表">
-                <appceptanceForm></appceptanceForm>
+                <appceptanceForm :disabled="approvalFormDisabled" :detail="detail"></appceptanceForm>
             </el-tab-pane>
             <el-tab-pane label="打印表单">定时任务补偿</el-tab-pane>
             <el-tab-pane label="材料表单">定时任务补偿</el-tab-pane>
@@ -28,16 +28,37 @@
   import approvalForm from './approval-form'
 
   export default {
-    name: 'todo-create',
+    name: 'detail-page',
     components: {
       applicationForm,
       approvalForm,
       appceptanceForm
     },
+    props:{
+      applicationFormDisabled: {
+        type: Boolean,
+        default: false
+      },
+      appceptanceFormDisabled: {
+        type: Boolean,
+        default: false
+      },
+      approvalFormDisabled: {
+        type: Boolean,
+        default: false
+      },
+      detail: {
+        type: Object,
+        default: undefined
+      }
+    },
+    created(){
+      console.log(this.$router)
+    },
     data() {
       return {
         active: 2,
-        form:{}
+
       }
     },
     methods: {}
