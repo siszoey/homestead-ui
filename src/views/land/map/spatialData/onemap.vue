17890f9856d68640cb707277dc959ddfa8e40ee5
lichen <lichenm@51jianku.com>
2020-02-05 14:05:15 +0800

    一张图横向工具条

--- src/views/land/map/spatialData/onemap.vue
+++ src/views/land/map/spatialData/onemap.vue
@@ -202,40 +202,7 @@
     </div>
 
     <div id="maponemap" class="mapDiv"></div>
-
-    <div class="toolbarContainer">
-      <div class="toolbar">
-        <div class="toolButton top">
-          <img :src="`${$baseUrl}image/mapicon/sldt.png`" />
-        </div>
-        <div class="toolButton bottom highlight">
-          <img :src="`${$baseUrl}image/mapicon/yxdt.png`" />
-        </div>
-      </div>
-      <div class="toolbar" style="margin-top:10px">
-        <div class="toolButton top">
-          <img :src="`${$baseUrl}image/mapicon/sjjc.png`" />
-        </div>
-        <div class="toolButton bottom">
-          <img :src="`${$baseUrl}image/mapicon/cggx.png`" />
-        </div>
-      </div>
-      <div class="toolbar" style="margin-top:10px">
-        <div :class="layerOn ?'toolButton top selected':'toolButton top '" @click="showLayer">
-          <img :src="`${$baseUrl}image/mapicon/tcgl.png`" />
-        </div>
-        <div class="toolButton">
-          <img :src="`${$baseUrl}image/mapicon/sqgl.png`" />
-        </div>
-        <div class="toolButton">
-          <img :src="`${$baseUrl}image/mapicon/fpdb.png`" />
-        </div>
-        <div class="toolButton bottom">
-          <img :src="`${$baseUrl}image/mapicon/zbdw.png`" />
-        </div>
-      </div>
-    </div>
-
+    <ToolBar></ToolBar>
     <LayerList style="position:absolute;top:180px;right:80px" v-show="layerOn"></LayerList>
     <el-dialog
       class="box-card right-side"
@@ -272,6 +239,7 @@
 // 引入Dialog可拖拽，注意文件所在目录。目前尚未发现引入的先后关系，若有再补充
 import "./components/directives.js";
 import LayerList from "./components/LayerList_ZJD";
+import ToolBar from "./components/toolbar";
 import BaseMap from "../spatialData/mapBase.js";
 
 import "ol/ol.css";
@@ -804,7 +772,8 @@ export default {
     };
   },
   components: {
-    LayerList
+    LayerList,
+    ToolBar
   },
   mounted() {
     this.$nextTick(function() {
@@ -821,12 +790,12 @@ export default {
 
       this.InitLayer("DT");
       //行政区置顶
-      
+
       if (this.zoomToZD) this.doZoomToZD();
-      else{
+      else {
         this.InitLayer("XZQ");
         this.XZQ.Layer.setZIndex(20);
-      } 
+      }
 
       // this.popup = new Overlay({
       //   element: document.getElementById("popup_onemap")
@@ -867,9 +836,6 @@ export default {
   },
 
   methods: {
-    showLayer() {
-      //this.layerOn = !this.layerOn;
-    },
     closeCard() {
       //document.getElementById("popup_onemap").style.visibility = "hidden";
       this.dialogVisible = false;
@@ -1160,48 +1126,6 @@ export default {
   position: absolute;
   left: 0px;
   top: 0px;
-}
-
-.toolbarContainer {
-  right: 30px;
-  top: 10px;
-  position: absolute;
-}
-.toolbar {
-  width: 40px;
-  z-index: 10;
-  box-shadow: 0px 0px 1px gray;
-  border-radius: 5px;
-  .toolButton {
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    opacity: 0.7;
-    background-color: gray;
-
-    &:hover {
-      opacity: 1;
-      background-color: white;
-    }
-  }
-  .highlight {
-    opacity: 1;
-    background-color: rgba(128, 128, 128, 0.5);
-  }
-  .selected {
-    opacity: 1;
-    background-color: white;
-  }
-  .top {
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
-  }
-  .bottom {
-    border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
-  }
 }
 
 .listItem {
